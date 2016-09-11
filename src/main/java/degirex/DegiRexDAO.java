610ecc02604e928e172b3a717ec42434a2833e16
ganesh-r <gar2304>
2016-09-11 16:07:18 +0530

    Commi all

---
+++ src/main/java/degirex/DegiRexDAO.java
@@ -0,0 +1,21 @@
+package degirex;
+
+public class DegiRexDAO {
+
+	String person;
+	String storage;
+	public String getPerson() {
+		return person;
+	}
+	public void setPerson(String person) {
+		this.person = person;
+	}
+	public String getStorage() {
+		return storage;
+	}
+	public void setStorage(String storage) {
+		this.storage = storage;
+	}
+	
+
+}
---
+++ src/test/java/degirex/TestLaunchScanner.java
@@ -0,0 +1,16 @@
+package degirex;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import junit.framework.TestCase;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources")
+public class TestLaunchScanner extends TestCase {
+
+
+	
+	
+}
